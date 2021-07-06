a6e3af8add1ade892f2c06296936f1f49b995c70
Stephanie Kamen <st.kamen@gmx.de>
2021-07-06 21:42:03 +0200

    studip coaching challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# 1.p >form with an input,user can type question to ask Coach
+# 2. After submitting form, user is redirected to 2nd page
+## will see question and the coach answer.
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/ask', to: 'pages#ask'
+  get '/answer', to: 'pages#answer'
 end
